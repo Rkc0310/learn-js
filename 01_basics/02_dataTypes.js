f48cf81ad110a562045f24e22e63e0f3dba1479a
Roshan <114645583+Rkc0310@users.noreply.github.com>
2025-06-30 08:04:41 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3); will not work in nodejs we are using nodejs not browser
+
+let name = "Roshan";
+let age =18;
+let isLoggedIn = false;
+
+// number => 2 to power 53 
+//bigInt  => more than 2 to power 53 
+//string => ""
+//boolean => true/false
+//null => emapty value standalone value
+//undefined => not defined/ not assigned
+
+//symbol => unique
+
+
+//object => key value pair 
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
