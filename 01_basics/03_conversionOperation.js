0c91fc3e9c9ae1e673b09a7c71e508656b3d7e22
Roshan <114645583+Rkc0310@users.noreply.github.com>
2025-06-30 08:05:48 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+//convert to Number.............
+let score = true;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//conver a number "33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+//convert to Boolean.............
+let isLoggedIn = 1;
+
+let boolleanIsLoggedIn = Boolean (isLoggedIn);
+console.log(boolleanIsLoggedIn);
+
+// 1 => true; 0=> false
+//"" => false;
+//"Roshan" => true;
+
+//convert to String.............
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+  
+// "33" => 33 
+// "33ABC" => NaN
+// true => 1; false => 0
