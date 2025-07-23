5a7f9f6ae9d27e1cec0746b7c7cb8decf4b0f650
Roshan <114645583+Rkc0310@users.noreply.github.com>
2025-07-23 11:36:22 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,56 @@
+//switch case 
+//syntax 
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3;
+switch (month) {
+    case 1:
+        console.log("januray");
+        break;
+
+    case 2:
+        console.log("fed");
+        break;
+
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("aprail");
+        break;
+
+    default: 
+      console.log("default value");  
+        break;
+}
+
+// if we have a string value
+const months = "feb"
+switch (months) {
+    case "january":
+        console.log("january");
+        break;
+
+         case "feb":
+        console.log("fed");
+        break;
+
+         case "march":
+        console.log("march");
+        break;
+
+    default:
+        console.log("default value print");
+        break;
+}
+
+
+
