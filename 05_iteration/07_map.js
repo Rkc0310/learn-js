b1776d6b746faffff9d174bc7bcc1a4e2c50cd1c
Roshan <114645583+Rkc0310@users.noreply.github.com>
2025-07-27 10:12:31 +0000

    map()

---
+++ 05_iteration/07_map.js
@@ -0,0 +1,18 @@
+//map is used to iterate over an array and return a new array with the results of calling a provided function on every element in the calling array.
+// It does not modify the original array, but instead creates a new one based on the transformation
+// we can use map to get the values from an array based on  a condition
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const myNums = myNumbers.map((num)=> num +10)             // we can use map to get the values from an array based on a condition without curly braces no need to return
+
+const myNums = myNumbers.map((num)=>{    // we can use map to get the values from an array based on a condition with curly braces we need to return
+
+return num * 10}).map       // returns a new array with the results of calling a provided function on every element in the calling array
+((num)=>
+{
+    return num +1        
+}).filter((num)=>{       //using a chain of methods  map after map  and filter the results
+    return num >= 40
+})
+console.log(myNums);
