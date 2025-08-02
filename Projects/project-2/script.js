c6180d27f960cc80ff35ddc3656108e5ae967897
Roshan <114645583+Rkc0310@users.noreply.github.com>
2025-08-02 15:25:05 +0000

    Js2

---
+++ Projects/project-2/script.js
@@ -0,0 +1,32 @@
+console.log("Roshan");
+
+const form = document.querySelector('form')
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+     const weight = parseInt(document.querySelector('#weight').value);
+      const results = document.querySelector('#results')
+
+      if (height === '' || height<0 || isNaN(height)){
+        results.innerHTML = `Please give a valid height ${height}`;
+      }
+      else if (weight === '' || weight<0 || isNaN(weight)){
+        results.innerHTML = `Please give a valid weight ${weight}`
+      }
+      else{
+        const bmi = (weight / ((height * height)/10000)).toFixed(2);
+        
+        if(bmi < 18.6 && bmi > 0 ){
+          results.innerHTML = `<span>${bmi} Now You Are Under Weight</span>`
+        }
+        else if(bmi > 18.6 && bmi <= 24.9){
+          results.innerHTML = `<span>${bmi} Now You Are Normal Range</span>`
+        }
+        else{
+          (bmi > 24.9)
+             results.innerHTML = `<span>${bmi} Overweight</span>`
+        }
+      }
+});
