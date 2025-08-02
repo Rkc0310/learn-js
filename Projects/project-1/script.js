87b46f50965066386fa4787da29547bc55fb1af7
Roshan <114645583+Rkc0310@users.noreply.github.com>
2025-08-02 07:00:11 +0000

    js

---
+++ Projects/project-1/script.js
@@ -0,0 +1,24 @@
+console.log("Roshan")
+
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach((btn)=>{
+console.log(btn)
+btn.addEventListener('click', function(el){
+  console.log(el)
+  console.log(el.target)
+if(el.target.id === 'grey'){
+  body.style.backgroundColor = el.target.id
+}
+if(el.target.id === 'white'){
+  body.style.backgroundColor = el.target.id
+}
+if(el.target.id === 'blue'){
+  body.style.backgroundColor = el.target.id
+}
+if(el.target.id === 'yellow'){
+  body.style.backgroundColor = el.target.id
+}
+})
+})
